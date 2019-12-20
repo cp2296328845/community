5101389b328eb642ea66a0c3bb464467c19063e8
Administrator <2296328845@qq.com>
2019-12-20 19:45:36 +0800

    add bootstrap

--- src/main/java/com/example/community/controller/HelloController.java
+++ src/main/java/com/example/community/controller/HelloController.java
@@ -13,5 +13,7 @@ public class HelloController {
     public String hello(@RequestParam(name = "name") String name, Model model){
         model.addAttribute("name",name);
         return "hello";
+
+
     }
 }
